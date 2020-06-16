36e47c41c37102fc3636e3f2e350b655a76f40c3
bculo <culobozo@gmail.com>
2020-06-16 21:55:45 +0200

    Automapper added

---
+++ ProcessExplorer/ProcessExplorer.Application/Configurations/MapperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProcessExplorer.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ProcessExplorer.Application.Configurations
+{
+    public class MapperConfiguration : IInstallation
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
